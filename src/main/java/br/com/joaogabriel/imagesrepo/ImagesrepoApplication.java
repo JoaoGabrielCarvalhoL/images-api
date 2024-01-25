dac5452dd3fae76c2b957024df555fa5a9835e50
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2024-01-25 12:22:30 -0300

    Started project

---
+++ src/main/java/br/com/joaogabriel/imagesrepo/ImagesrepoApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaogabriel.imagesrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagesrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaogabriel/imagesrepo/ImagesrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaogabriel.imagesrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
